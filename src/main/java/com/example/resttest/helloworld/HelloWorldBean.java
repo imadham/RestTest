918c4e550e85a5dbd2dc56062d1151cafbda11c7
hamameh.i <hamameh.i@basis-ic.ru>
2025-03-25 15:25:44 +0300

    using path Variable

--- src/main/java/com/example/resttest/helloworld/HelloWorldBean.java
+++ src/main/java/com/example/resttest/helloworld/HelloWorldBean.java
@@ -1,2 +1,17 @@
-package com.example.resttest.helloworld;public class HelloWorldBean {
+package com.example.resttest.helloworld;
+
+public class HelloWorldBean {
+
+    String text;
+    public HelloWorldBean(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- src/main/java/com/example/resttest/helloworld/HelloWorldController.java
+++ src/main/java/com/example/resttest/helloworld/HelloWorldController.java
@@ -1,2 +1,23 @@
-package com.example.resttest.helloworld;public class HelloWorldController {
+package com.example.resttest.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("hello-world")
+    public String getHelloWorld(){
+        return new String("Hello World");
+    }
+    @GetMapping("hello-world-bean")
+    public HelloWorldBean getHelloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("hello-world-bean/path-variable/{name}")
+    public HelloWorldBean getHelloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean("Hello World"+": "+name);
+    }
 }
